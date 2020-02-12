a88455d92100ca2d21cb12c3f00e018a5d0d9212
Reed Reese-Steiner <arleycat@gmail.com>
2020-02-11 21:34:04 -0800

    attempt to get a simple ajax setup between browser and node.js script

--- web_ui/gui.js
+++ web_ui/gui.js
@@ -1,3 +1,10 @@
 //import * as zmq from 'jszmq';
+document.body.innerHTML += "POSTing"
+
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", "localhost:8000", true);
+//xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhttp.send("hello?"); 
+
 let d = new Date();
 document.body.innerHTML += "<p>date: " + d + "</h1>"
---
+++ web_ui/server.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var server = http.createServer ( function(request,response){
+
+  response.writeHead(200,{"Content-Type":"text\plain"});
+  if(request.method == "GET")
+  {
+    response.end("received GET request.")
+    console.log("received GET request.")
+  }
+  else if(request.method == "POST")
+  {
+    response.end("received POST request.");
+    console.log("received POST request.")
+  }
+  else
+  {
+    response.end("Undefined request .");
+  }
+});
+
+server.listen(8000);
+console.log("Server running on port 8000");
+
