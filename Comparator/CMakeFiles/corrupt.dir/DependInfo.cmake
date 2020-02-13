769bba345f89ac3b1938d1b1030480c441290bbe
ebrahimk <ebrahimk@oregonstate.edu>
2020-02-12 16:35:04 -0800

    reading serialized data over tcp socket and reconstructing image complete, next step is to break program into threads which share queues and refactor code

---
+++ Comparator/CMakeFiles/corrupt.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Code/capstone_camera_validation/Corruption/Compare.cpp" "/home/pi/Code/capstone_camera_validation/Corruption/CMakeFiles/corrupt.dir/Compare.cpp.o"
+  "/home/pi/Code/capstone_camera_validation/Corruption/filters/Distortion.cpp" "/home/pi/Code/capstone_camera_validation/Corruption/CMakeFiles/corrupt.dir/filters/Distortion.cpp.o"
+  "/home/pi/Code/capstone_camera_validation/Corruption/filters/Freeze.cpp" "/home/pi/Code/capstone_camera_validation/Corruption/CMakeFiles/corrupt.dir/filters/Freeze.cpp.o"
+  "/home/pi/Code/capstone_camera_validation/Corruption/filters/Translate.cpp" "/home/pi/Code/capstone_camera_validation/Corruption/CMakeFiles/corrupt.dir/filters/Translate.cpp.o"
+  "/home/pi/Code/capstone_camera_validation/Corruption/filters/White.cpp" "/home/pi/Code/capstone_camera_validation/Corruption/CMakeFiles/corrupt.dir/filters/White.cpp.o"
+  "/home/pi/Code/capstone_camera_validation/Corruption/main.cpp" "/home/pi/Code/capstone_camera_validation/Corruption/CMakeFiles/corrupt.dir/main.cpp.o"
+  "/home/pi/Code/capstone_camera_validation/Corruption/proto/packet.pb.cc" "/home/pi/Code/capstone_camera_validation/Corruption/CMakeFiles/corrupt.dir/proto/packet.pb.cc.o"
+  "/home/pi/Code/capstone_camera_validation/Corruption/socket/PracticalSocket.cpp" "/home/pi/Code/capstone_camera_validation/Corruption/CMakeFiles/corrupt.dir/socket/PracticalSocket.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
